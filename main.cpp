845a45f721fb32e18743d47ad18ddc71d91f4ba3
arthur <arthur@meetprizm.com>
2017-08-09 00:31:26 +0200

    first draft

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
+#include "SBUS.h"
+
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World" << endl;
+    uint16_t channels[16];
+
+    if(argc < 2)
+    {
+        cout << "please give tty name" << endl;
+        return -1;
+    }
+
+    SBUS::SBUS sbusport(argv[1]);
+
+    sbusport.begin();
+    channels[0] = 0;
+    channels[1] = 50;
+    channels[2] = 1000;
+    channels[3] = 2000;
+    sbusport.write(channels);
+
     return 0;
 }
