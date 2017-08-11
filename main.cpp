7214867b6c2d9a57ae714f12878c70b7dd1f7c78
arthur <arthur@meetprizm.com>
2017-08-12 00:03:02 +0200

    adding json file settings management

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <inttypes.h>
 #include <stdio.h>
 #include <termios.h>
@@ -6,8 +7,16 @@
 #include <fcntl.h>
 
 #include "SBUS.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 using namespace std;
+
+#define SBUS_MIN            11
+#define SBUS_MIDDLE         992
+#define SBUS_MAX            1977
+#define SETTINGS_FILENAME   "settings.json"
 
 int kbhit(void)
 {
@@ -36,30 +45,56 @@ int kbhit(void)
   return 0;
 }
 
-#define MIN 11
-#define MIDDLE 992
-#define MAX 1977
-
 int main(int argc, char *argv[])
 {
     char key;
     bool armed = false;
-    uint16_t throttle = MIN;
-    uint16_t yaw = MIDDLE, pitch = MIDDLE, roll = MIDDLE;
+    uint16_t throttle = SBUS_MIN;
+    uint16_t yaw = SBUS_MIDDLE, pitch = SBUS_MIDDLE, roll = SBUS_MIDDLE;
     uint16_t channels[16];
     int step = 100;
 
+    //*************// check args //*************//
     if(argc < 2)
     {
         cout << "please give tty name" << endl;
         return -1;
     }
 
-    for(int i=0;i<16;i++)
-        channels[i] = MIN;
+    //*************// load settings file //*************//
+    if(access( SETTINGS_FILENAME, F_OK ) == -1)
+    {
+        // create settings file
+        ofstream settings(SETTINGS_FILENAME);
+        json json_settings = {
+            {"init_values", {
+                {"throttle", throttle},
+                {"yaw", yaw},
+                {"pitch", pitch},
+                {"roll", roll}
+            }}
+        };
 
+        cout << "Writing initial JSON settings file to " << SETTINGS_FILENAME << endl;
+
+        settings << json_settings.dump(4) << endl;
+        settings.close();
+    }
+    else
+    {
+        // read from settings file
+        ifstream settings(SETTINGS_FILENAME);
+        json json_settings;
+        settings >> json_settings;
+
+        throttle = json_settings["init_values"]["throttle"];
+        yaw = json_settings["init_values"]["yaw"];
+        pitch = json_settings["init_values"]["pitch"];
+        roll = json_settings["init_values"]["roll"];
+    }
+
+    //*************// main loop //*************//
     SBUS::SBUS sbusport(argv[1]);
-
     sbusport.begin();
 
     while(true)
