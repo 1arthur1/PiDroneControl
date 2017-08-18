a5c1b49e45b74925faf47653f2c3f930b469c76d
arthur <arthur@meetprizm.com>
2017-08-18 13:19:36 +0200

    adding network control

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PiDroneControl
+
+#Libraries used
+
+Networking: https://github.com/Smilex/zed_net
+
+JSON: https://github.com/nlohmann/json
+Network: https://github.com/dermesser/libsocket.git
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <libsocket/inetserverdgram.hpp>
 
 #include "SBUS.h"
 #include "json.hpp"
@@ -53,6 +54,9 @@ int main(int argc, char *argv[])
     uint16_t yaw = SBUS_MIDDLE, pitch = SBUS_MIDDLE, roll = SBUS_MIDDLE;
     uint16_t channels[16];
     int step = 100;
+    string ntwrk_from_port, ntwrk_from;
+    int ntwrk_bytes;
+    char ntwrk_buf[1024];
 
     //*************// check args //*************//
     if(argc < 2)
@@ -93,53 +97,75 @@ int main(int argc, char *argv[])
         roll = json_settings["init_values"]["roll"];
     }
 
-    //*************// main loop //*************//
-    SBUS::SBUS sbusport(argv[1]);
-    sbusport.begin();
-
-    while(true)
+    //*************// network init //*************//
+    try
     {
-        if(kbhit())
+        libsocket::inet_dgram_server sock("192.168.254.146", "4000", LIBSOCKET_IPv4, SOCK_NONBLOCK); // IPv4 and IPv6
+
+        //*************// main loop //*************//
+        SBUS::SBUS sbusport(argv[1]);
+        sbusport.begin();
+
+        while(true)
         {
-            key = getchar();
+            //*************// network control //*************//
+            if((ntwrk_bytes = sock.rcvfrom(ntwrk_buf, 1024, ntwrk_from, ntwrk_from_port)) != -1)
+            {
+                memcpy(&throttle, ntwrk_buf, sizeof(uint16_t));
+                memcpy(&yaw, ntwrk_buf+sizeof(uint16_t), sizeof(uint16_t));
+                memcpy(&pitch, ntwrk_buf+2*sizeof(uint16_t), sizeof(uint16_t));
+                memcpy(&roll, ntwrk_buf+3*sizeof(uint16_t), sizeof(uint16_t));
+                memcpy(&armed, ntwrk_buf+4*sizeof(uint16_t), sizeof(uint16_t));
 
-            if(key == 27) // ESC
-                break;
+                cout << "network receive, a t y p r: " << armed << " " << throttle << " " << yaw << " " << pitch << " " << roll << endl;
+            }
 
-            if(key == 'e')
-                throttle = (throttle+step) > SBUS_MAX ? SBUS_MAX : throttle+step;
-            if(key == 'a')
-                throttle = (throttle-step) < SBUS_MIN ? SBUS_MIN : throttle-step;
+            //*************// keyboard control //*************//
+            if(kbhit())
+            {
+                key = getchar();
 
-            if(key == 'z')
-                pitch = (pitch+step) > SBUS_MAX ? SBUS_MAX : pitch+step;
-            if(key == 's')
-                pitch = (pitch-step) < SBUS_MIN ? SBUS_MIN : pitch-step;
+                if(key == 27) // ESC
+                    break;
 
-            if(key == 'd')
-                roll = (roll+step) > SBUS_MAX ? SBUS_MAX : roll+step;
-            if(key == 'q')
-                roll = (roll-step) < SBUS_MIN ? SBUS_MIN : roll-step;
+                if(key == 'e')
+                    throttle = (throttle+step) > SBUS_MAX ? SBUS_MAX : throttle+step;
+                if(key == 'a')
+                    throttle = (throttle-step) < SBUS_MIN ? SBUS_MIN : throttle-step;
 
-            if(key == 'c')
-                yaw = (yaw+step) > SBUS_MAX ? SBUS_MAX : yaw+step;
-            if(key == 'w')
-                yaw = (yaw-step) < SBUS_MIN ? SBUS_MIN : yaw-step;
+                if(key == 'z')
+                    pitch = (pitch+step) > SBUS_MAX ? SBUS_MAX : pitch+step;
+                if(key == 's')
+                    pitch = (pitch-step) < SBUS_MIN ? SBUS_MIN : pitch-step;
 
-            if(key == 'x')
-                armed = !armed;
+                if(key == 'd')
+                    roll = (roll+step) > SBUS_MAX ? SBUS_MAX : roll+step;
+                if(key == 'q')
+                    roll = (roll-step) < SBUS_MIN ? SBUS_MIN : roll-step;
 
-            cout << "a t y p r: " << armed << " " << throttle << " " << yaw << " " << pitch << " " << roll << endl;
+                if(key == 'c')
+                    yaw = (yaw+step) > SBUS_MAX ? SBUS_MAX : yaw+step;
+                if(key == 'w')
+                    yaw = (yaw-step) < SBUS_MIN ? SBUS_MIN : yaw-step;
+
+                if(key == 'x')
+                    armed = !armed;
+
+                cout << "a t y p r: " << armed << " " << throttle << " " << yaw << " " << pitch << " " << roll << endl;
+            }
+
+            channels[0] = roll;
+            channels[1] = pitch;
+            channels[3] = yaw;
+            channels[2] = throttle;
+            channels[4] = armed ? 992 : 11;
+            sbusport.write(channels);
+
+            usleep(7000); // send every 7ms (SBUS high speed mode)
         }
-
-        channels[0] = roll;
-        channels[1] = pitch;
-        channels[3] = yaw;
-        channels[2] = throttle;
-        channels[4] = armed ? 992 : 11;
-        sbusport.write(channels);
-
-        usleep(7000); // send every 7ms (SBUS high speed mode)
+    } catch (const libsocket::socket_exception& exc)
+    {
+        cerr << exc.mesg;
     }
 
     cout << "exiting" << endl;
